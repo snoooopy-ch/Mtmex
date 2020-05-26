3284436a1370495ff256fb43a7a3e25e7149fd86
phoenix-gl <guangli901030@hotmail.com>
2020-05-27 03:39:15 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ResService} from './res.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mtmex';
+  constructor(private resService: ResService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { RightPanelComponent } from './right-panel/right-panel.component';
+import { HeaderComponent } from './left-panel/header/header.component';
+import { ContentComponent } from './left-panel/content/content.component';
+import { StatusComponent } from './left-panel/status/status.component';
+import { ResComponent } from './left-panel/res/res.component';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    LeftPanelComponent,
+    RightPanelComponent,
+    HeaderComponent,
+    ContentComponent,
+    StatusComponent,
+    ResComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
