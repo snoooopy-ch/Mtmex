ae4245e16deec37a4a9bd091d258f9fed827fe7a
Rolando Yang <rolando.trujillo@protonmail.com>
2021-03-03 02:48:22 -0800

    fix errors 11 to 16

--- src/app/left-panel/status/status.component.ts
+++ src/app/left-panel/status/status.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {ResService} from '../../res.service';
 
 @Component({
@@ -8,55 +8,59 @@ import {ResService} from '../../res.service';
 })
 export class StatusComponent implements OnInit, OnDestroy {
 
-  totalCount;
-  settings;
-  @Input() tabIndex;
-  selectCount = 0;
-  candi1Count = 0;
-  candi2Count = 0;
-  candi3Count = 0;
-  candi4Count = 0;
-  tabTitle;
+  @Input()
+  public tabIndex;
+  @Input()
+  public tabInfo: any = {};
   public subscribers: any = {};
-  constructor(private resService: ResService, private cdRef: ChangeDetectorRef) { }
+  constructor(private resService: ResService) { }
 
   ngOnInit(): void {
-    this.subscribers.selectedRes = this.resService.selectedRes.subscribe(value => {
-      if (this.tabIndex === value.tabIndex && value.statusToken) {
-        this.selectCount = value.select;
-        this.candi1Count = value.candi1;
-        this.candi2Count = value.candi2;
-        this.candi3Count = value.candi3;
-        this.candi4Count = value.candi4;
-        this.cdRef.detectChanges();
-        value.statusToken = false;
-      }
-    });
-    this.subscribers.totalRes = this.resService.totalRes.subscribe(value => {
-      if (this.tabIndex === value.tabIndex && value.statusToken){
-        this.totalCount = value.totalCount;
-        this.tabTitle = value.title;
-        value.statusToken = false;
-      }
-    });
+    this.tabInfo = {
+      tabTitle: '',
+      selectCount: 0,
+      candi1Count: 0,
+      candi2Count: 0,
+      candi3Count: 0,
+      candi4Count: 0,
+      totalCount: 0
+    };
+    // this.subscribers.selectedRes = this.resService.selectedRes.subscribe(value => {
+    //   if (this.tabIndex === value.tabIndex && value.statusToken) {
+    //     this.selectCount = value.select;
+    //     this.candi1Count = value.candi1;
+    //     this.candi2Count = value.candi2;
+    //     this.candi3Count = value.candi3;
+    //     this.candi4Count = value.candi4;
+    //     this.cdRef.detectChanges();
+    //     value.statusToken = false;
+    //   }
+    // });
+    // this.subscribers.totalRes = this.resService.totalRes.subscribe(value => {
+    //   if (this.tabIndex === value.tabIndex && value.statusToken){
+    //     this.totalCount = value.totalCount;
+    //     this.tabTitle = value.title;
+    //     value.statusToken = false;
+    //   }
+    // });
 
-    this.subscribers.status = this.resService.status.subscribe((value) => {
-      if (this.tabIndex === value.tabIndex) {
-        this.selectCount = value.data.selectCount;
-        this.totalCount = value.data.totalCount;
-        this.candi1Count = value.data.candi1Count;
-        this.candi2Count = value.data.candi2Count;
-        this.candi3Count = value.data.candi3Count;
-        this.candi4Count = value.data.candi4Count;
-      }
-    });
+    // this.subscribers.status = this.resService.status.subscribe((value) => {
+    //   if (this.tabIndex === value.tabIndex) {
+    //     this.selectCount = value.data.selectCount;
+    //     this.totalCount = value.data.totalCount;
+    //     this.candi1Count = value.data.candi1Count;
+    //     this.candi2Count = value.data.candi2Count;
+    //     this.candi3Count = value.data.candi3Count;
+    //     this.candi4Count = value.data.candi4Count;
+    //   }
+    // });
 
   }
 
   ngOnDestroy(){
-    this.subscribers.totalRes.unsubscribe();
-    this.subscribers.selectedRes.unsubscribe();
-    this.subscribers.status.unsubscribe();
+    // this.subscribers.totalRes.unsubscribe();
+    // this.subscribers.selectedRes.unsubscribe();
+    // this.subscribers.status.unsubscribe();
   }
 
   printHtmlTagHandler() {
